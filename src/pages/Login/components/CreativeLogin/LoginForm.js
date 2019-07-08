3f2cf7957a21d0ed9c6a80852fd6b453d6bcea95
QuXiaoMing <xm726@qq.com>
2019-07-08 20:02:45 +0800

    【修改】登录

--- src/pages/Login/components/CreativeLogin/LoginForm.js
+++ src/pages/Login/components/CreativeLogin/LoginForm.js
@@ -1,10 +1,10 @@
 /* eslint react/no-string-refs:0 */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {withRouter} from 'react-router-dom';
-import {Message} from '@alifd/next';
-import {login} from '@/api/login';
-import {observer} from 'mobx-react';
+import { withRouter } from 'react-router-dom';
+import { Message } from '@alifd/next';
+import { login } from '@/api/login';
+import { observer } from 'mobx-react';
 import userStore from '@/store/User';
 import AuthForm from './AuthForm';
 
@@ -16,9 +16,9 @@ export default class LoginForm extends Component {
   static defaultProps = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      userStore.logout();
-    }, 1000);
+    // setTimeout(() => {
+    //   userStore.logout();
+    // }, 1000);
   }
 
   formChange = value => {
@@ -33,11 +33,15 @@ export default class LoginForm extends Component {
 
     console.log('values:', values);
     let ret = await login(values);
+    console.log('ret', ret);
     if (ret.isSuccess) {
       Message.success('登录成功');
       // window.aaa = this.props;
-      userStore.userInfo = {token: ret.data.data, ...values};
-      this.props.history.push('/');
+      userStore.userInfo = { token: ret.data, ...values };
+      setTimeout(() => {
+        console.log('userStore', ret.data);
+        this.props.history.push('/');
+      });
     }
     // 登录成功后做对应的逻辑处理
   };
@@ -95,7 +99,7 @@ export default class LoginForm extends Component {
       checkbox: false
     };
 
-    const links = [{to: '/register', text: '立即注册'}, {to: '/forgetpassword', text: '找回密码'}];
+    const links = [{ to: '/register', text: '立即注册' }, { to: '/forgetpassword', text: '找回密码' }];
 
     return (
       <div>
